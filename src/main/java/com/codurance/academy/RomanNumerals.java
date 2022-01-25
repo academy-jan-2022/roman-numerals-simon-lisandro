c7aa01a7ddc3f2ea083f172a5d64c4058a748d7f
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-25 10:23:27 +0100

    green

--- src/main/java/com/codurance/academy/RomanNumerals.java
+++ src/main/java/com/codurance/academy/RomanNumerals.java
@@ -2,6 +2,9 @@ package com.codurance.academy;
 
 public class RomanNumerals {
     public String convert(int decimalNumber) {
+        if(decimalNumber == 2) {
+            return "II";
+        }
         return "I";
     }
 }
